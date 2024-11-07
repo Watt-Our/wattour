5b0c54c4db57c20564ee8adc2ff60074fc5ce09e
Alex Tseng <134972440+alexrtseng@users.noreply.github.com>
2024-11-07 11:39:18 -0500

    Fill out setup

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='funniest',
       version='0.1',
       description='Internal Watt-Our package for optimization methods',
-      url='http://github.com/storborg/funniest',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
+      url='https://github.com/Watt-Our/wattour_optimization.git',
+      author='Watt-Our Team',
+      author_email='at2720@princeton.edu',
       license='Proprietary - Watt-Our',
       packages=['wattour_optimization'],
       zip_safe=False)
