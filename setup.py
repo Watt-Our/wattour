5278f45d1cb6f925de457b2f49e3b942ebbcd07c
Carter Costic <cartercostic@gmail.com>
2025-02-11 16:13:29 -0500

    formatting setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
-setup(name='funniest',
-      version='0.1',
-      description='Internal Watt-Our package for optimization methods',
-      url='https://github.com/Watt-Our/wattour_optimization.git',
-      author='Watt-Our Team',
-      author_email='at2720@princeton.edu',
-      license='Proprietary - Watt-Our',
-      packages=['wattour_optimization'],
-      zip_safe=False)
+setup(
+    name="funniest",
+    version="0.1",
+    description="Internal Watt-Our package for optimization methods",
+    url="https://github.com/Watt-Our/wattour_optimization.git",
+    author="Watt-Our Team",
+    author_email="at2720@princeton.edu",
+    license="Proprietary - Watt-Our",
+    packages=["wattour_optimization"],
+    zip_safe=False,
+)
